e21dc2ca1b550cea1869cde8d9c39bef9b9a1a4e
Hans Mündelein <hmuendel@gmail.com>
2019-08-16 15:30:21 +0200

    Make it work

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM python:alpine
 RUN pip install requests
 
 COPY reaper.py .
-CMD ["/reaper.py"]
+CMD ["python",  "./reaper.py"]
 
 
 
--- reaper.py
+++ reaper.py
@@ -1,14 +1,15 @@
 import requests
 from time import sleep
 
- while True:
-   print(':fire: fueling chainsaw')
-   containers = requests.get('http://localhost:7777/containers').json()['Handles']
-   print(f':telescope: looking for zombies')
-   for container in containers:
-     status = requests.get(f'http://localhost:7777/containers/{container}/info').status_code
-     if status == 500:
-       # give garden 1 sec to kill the zombie
-       print(':zombie: :gun: Die zombie, die!')
-       requests.put(f'http://localhost:7777/containers/{container}/grace_time', data=f'{1_000_000_000}').status_code
-   sleep(30)
+while True:
+    print(':fire: fueling chainsaw')
+    containers = requests.get('http://localhost:7777/containers').json()['Handles']
+    print(f':telescope: looking for zombies')
+    for container in containers:
+        status = requests.get(f'http://localhost:7777/containers/{container}/info').status_code
+        if status == 500:
+            # give garden 1 sec to kill the zombie
+            print(':zombie: :gun: Die zombie, die!')
+            requests.put(f'http://localhost:7777/containers/{container}/grace_time',
+                         data=f'{1_000_000_000}').status_code
+    sleep(30)
