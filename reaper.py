609ff5ce4b2605630da5fdc082b15b7fe8497e00
Hans Mündelein <hmuendel@gmail.com>
2019-08-16 15:54:01 +0200

    Fix log emojis

--- reaper.py
+++ reaper.py
@@ -2,14 +2,14 @@ import requests
 from time import sleep
 
 while True:
-    print(':fire: fueling chainsaw')
+    print('🔥 fueling chainsaw')
     containers = requests.get('http://localhost:7777/containers').json()['Handles']
-    print(f':telescope: looking for zombies')
+    print(f'🔭 looking for zombies')
     for container in containers:
         status = requests.get(f'http://localhost:7777/containers/{container}/info').status_code
         if status == 500:
             # give garden 1 sec to kill the zombie
-            print(':zombie: :gun: Die zombie, die!')
+            print('🧟‍♂️🧟‍♀️ 🔫 Die zombie, die!')
             requests.put(f'http://localhost:7777/containers/{container}/grace_time',
                          data=f'{1_000_000_000}').status_code
     sleep(30)
