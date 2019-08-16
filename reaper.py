978140ed14e49b81230a4654b13f5ec78655ead0
Hans Mündelein <hmuendel@gmail.com>
2019-08-16 17:15:53 +0200

    Add timestamps and relax cleaning stress a little

--- reaper.py
+++ reaper.py
@@ -1,15 +1,18 @@
 import requests
 from time import sleep
+from datetime import datetime
 
 while True:
-    print('🔥 fueling chainsaw')
+    print(f'{datetime.now()} 🔥 fueling chainsaw')
     containers = requests.get('http://localhost:7777/containers').json()['Handles']
-    print(f'🔭 looking for zombies')
+    print(f'{datetime.now()}  🔭 looking for zombies')
     for container in containers:
         status = requests.get(f'http://localhost:7777/containers/{container}/info').status_code
         if status == 500:
             # give garden 1 sec to kill the zombie
-            print('🧟‍♂️🧟‍♀️ 🔫 Die zombie, die!')
+            print(f'{datetime.now()}  🧟🧟‍️ 🔫 Die zombie, die!')
             requests.put(f'http://localhost:7777/containers/{container}/grace_time',
                          data=f'{1_000_000_000}').status_code
-    sleep(30)
+            # relax a little to not stress garden to much
+            sleep(1)
+    sleep(60)
